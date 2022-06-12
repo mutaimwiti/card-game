d047b02b95defee3105c3aa9cc3e6ce95ab4d5fd
Mutai Mwiti <mutaimwiti40@gmail.com>
2022-06-13 01:53:24 +0300

    Add general docs

---
+++ index.js
@@ -0,0 +1,11 @@
+// We start with the deck of 52 cards, each uniquely numbered from 1 to 53
+// The deck is shuffled
+// We deal out those cards between the 2 players. Each player gets half the deck.
+// On each turn of the game, both players turn over their topmost card, and they compare the value of those cards.
+// The player with the higher valued card "wins" the round and gets a point. The two cards being compared are discarded.
+// Rounds are played until all the cards are discarded.
+// At the end of the game the player who has the most points wins.
+//
+// Output:
+// When each round is played you should print each player's card value along with an indication of which player won that round.
+// When all rounds are played you should print each player's final score along with an indication of which player won overall.
