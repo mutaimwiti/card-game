677e4bc4705738b0cb5227743021a134807608d6
Mutai Mwiti <mutaimwiti40@gmail.com>
2022-06-21 23:51:47 +0300

    Assert that run() calls Game start() method

--- index.test.js
+++ index.test.js
@@ -1,9 +1,16 @@
+import Game from "./src/Game";
 import { run } from "./index";
 
 describe('index.js',  function () {
     describe('run()',  () => {
+        const startSpy = jest.spyOn(Game.prototype, 'start');
+
         it('it should not blow up', function () {
             expect(() => run()).not.toThrow();
+        });
+
+        it('should fire Game start() method', function () {
+            expect(startSpy).toHaveBeenCalled();
         });
     });
 });
